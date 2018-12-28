0d1de31fa76ca24a1220b5e07272447ef5e5cd74
sh3riff <Dr.strgsh@gmail.com>
2018-12-28 16:14:48 +0200

    Design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order(params[:sort])
+    @posts = Post.all
     @post = Post.new
   end
 
